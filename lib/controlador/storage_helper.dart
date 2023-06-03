49a72990d68ffd1bebcff1f991801f7a03dd8708
Luis Daza <Alejogaba@gmail.com>
2023-06-02 22:01:52 -0500

    Funcion guardar imagen

--- lib/controlador/storage_helper.dart
+++ lib/controlador/storage_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'dart:html';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 import '../flutter_flow/flutter_flow_util.dart';
@@ -10,9 +11,42 @@ class StorageHelper {
   int dataIndex = base64Data.indexOf(',') + 1;
   String base64String = base64Data.substring(dataIndex);
   Uint8List bytes = base64.decode(base64String);
-// Crear una referencia al archivo en Firebase Storage
-final storageRef = FirebaseStorage.instance.ref().child('pqrs/$nombre');
-await storageRef.putData(bytes).whenComplete(() => print('Archivo subido correctamente'));
+  // Crear una referencia al archivo en Firebase Storage
+  final storageRef = FirebaseStorage.instance.ref().child('pqrs/$nombre');
+  await storageRef.putData(bytes).whenComplete(() => print('Archivo subido correctamente'));
   }
+
+Future<void> guardarImagenEnFirebaseStorage(Uint8List imagenBytes, String idFuncionario) async {
+  try {
+    final int limiteTamanio = 1024 * 1024; // 1 MB
+
+    // Comprimir la imagen si supera el límite de tamaño
+    if (imagenBytes.length > limiteTamanio) {
+      final compressedBytes = await FlutterImageCompress.compressWithList(
+        imagenBytes,
+        minHeight: 400,
+        minWidth: 400,
+        quality: 85,
+      );
+      imagenBytes = Uint8List.fromList(compressedBytes);
+    }
+
+    // Crear una referencia al archivo en Firebase Storage
+    final ref = FirebaseStorage.instance.ref().child('funcionarios/$idFuncionario.jpg');
+
+    // Subir los bytes de la imagen a Firebase Storage
+    final uploadTask = ref.putData(imagenBytes);
+
+    // Esperar a que se complete la carga del archivo
+    final snapshot = await uploadTask.whenComplete(() {});
+
+    // Obtener la URL de descarga del archivo
+    final downloadURL = await snapshot.ref.getDownloadURL();
+
+    print('Imagen guardada en Firebase Storage: $downloadURL');
+  } catch (error) {
+    print('Error al guardar la imagen en Firebase Storage: $error');
+  }
+}
   
 }
